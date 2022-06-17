84a90c643e60632bc77f1959beb80aa9e0d8a5fd
TedEngineer <tedvu184@gmail.com>
2022-06-17 23:59:18 +1000

    Remove eslint rule in precommit for now

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!');
+// middleware to add data from body to requests object
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: 9,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours/', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this url');
 });
 
 const port = 3000;
