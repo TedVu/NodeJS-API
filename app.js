1d5d39eec6e5bd8f7d1880bdcefc16cfdaef1aff
TedEngineer <tedvu184@gmail.com>
2022-06-18 23:41:16 +1000

    Add simple post request with data persistence through file

--- app.js
+++ app.js
@@ -20,8 +20,23 @@ app.get('/api/v1/tours/', (req, res) => {
 });
 
 app.post('/api/v1/tours/', (req, res) => {
-  console.log(req.body);
-  res.send('Done');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.post('/', (req, res) => {
