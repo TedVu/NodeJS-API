e11a127dea6559ee4b1b90e4142560346d0a0cd2
TedEngineer <tedvu184@gmail.com>
2022-06-20 23:54:14 +1000

    Refactor endpoints with route from express

--- app.js
+++ app.js
@@ -92,11 +92,16 @@ const deleteTour = function (req, res) {
   });
 };
 
-app.get('/api/v1/tours/', getAllTours);
-app.post('/api/v1/tours/', createTour);
 app.get('/api/v1/tours/:id', getTour);
 app.patch('api/v1/tours/:id', updateTour);
 app.delete('api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours/').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 
