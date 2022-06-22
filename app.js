761c03cd2c65c0b66d47dee26cffe4a344c647c0
TedEngineer <tedvu184@gmail.com>
2022-06-22 21:54:38 +1000

    Implement place holder route handler for user endpoints

--- app.js
+++ app.js
@@ -108,12 +108,53 @@ const deleteTour = function (req, res) {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
 app.route('/api/v1/tours/').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users/').get(getAllUsers).post(createUser);
+
+app.route('api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.route();
 
 const port = 3000;
 
