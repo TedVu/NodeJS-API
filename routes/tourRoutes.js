90752a82c798f98d37f268eeaab440cd56cf7b34
TedEngineer <tedvu184@gmail.com>
2022-06-26 15:22:17 +1000

    Add middleware code run when there is a id

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// middleware runs when id is presentt in the urls
+router.param('id', (req, res, next, id) => {
+  console.log(`Tour id is: ${id}`);
+  next();
+});
+
 router
   .route('/')
   .get(tourController.getAllTours)
