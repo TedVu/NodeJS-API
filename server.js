01e5253154abb7c5ba78f734a93b4fff287a8587
TedEngineer <tedvu184@gmail.com>
2022-06-30 00:07:03 +1000

    Fix connection problem between application and mongodb atlas

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE).then((con) => {
+  console.log(con.connections);
+  console.log('DB connection successful');
+});
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
