e8068cd396c60e12729a59f6d0d954cea78f7c99
TedEngineer <tedvu184@gmail.com>
2022-06-29 23:36:32 +1000

    Connect application with mongodb

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+mongoose.connect(process.env.DATABASE);
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
