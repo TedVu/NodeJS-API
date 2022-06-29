89e2a6b1aa685efe43655d6b3fee252ef39f89ae
TedEngineer <tedvu184@gmail.com>
2022-06-30 00:21:54 +1000

    Add rule to disable error of unused variable eslint

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ mongoose.connect(process.env.DATABASE).then((con) => {
 });
 const app = require('./app');
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
